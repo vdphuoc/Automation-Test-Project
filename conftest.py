e46c40ba07fcdd77c2bfd019c149c9aebeed4b78
Gnasche <phuoc.workspace@gmail.com>
2024-06-15 22:32:48 +0700

    change to headless mode to run pipeline

--- conftest.py
+++ conftest.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 @pytest.fixture(scope="function")
 def browser_context():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         yield page
